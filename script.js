d388c86b46f514c3e79c6f80746d2e96d5ef8c5c
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-19 01:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
+}
+
+class Employee extends Person{
+	constructor(name,age,jobTitle){
+		this.name=name;
+		this.age=age;
+		this.jobTitle=jobTitle;
+	}
+
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
+}
+
+// function Person(name, age) {}
+
+// function Employee(name, age, jobTitle) {}
 
 // Do not change code below this line
 window.Person = Person;
