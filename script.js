a33db8b60ad4d0f60a98d0b6815b6c06e2c31f19
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-13 01:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-// // complete this js code
-// class Person{
-// 	constructor(name, age){
-// 		this.name=name;
-// 		this.age=age;
-// 	}
+// complete this js code
+class Person{
+	constructor(name, age){
+		this.name=name;
+		this.age=age;
+	}
 
-// 	greet(){
-// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-// 	}
-// }
-
-// class Employee extends Person{
-// 	constructor(name,age,jobTitle){
-// 		this.name=name;
-// 		this.age=age;
-// 		this.jobTitle=jobTitle;
-// 	}
-
-// 	jobGreet(){
-// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-// 	}
-// }
-
-function Person(name, age) {
-	this.name = name;
-	this.age = age;
-}
-
-Person.prototype.greet = function(){
+	greet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+	}
 }
 
-function Employee(name, age, jobTitle) {
-	this.name = name;
-	this.age = age;
-	this.jobTitle = jobTitle;
+class Employee extends Person{
+	constructor(name,age,jobTitle){
+		this.name=name;
+		this.age=age;
+		this.jobTitle=jobTitle;
+	}
 
-	__proto__=Person;
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+	}
 }
 
-Employee.prototype.jobGreet = function(){
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
+// function Person(name, age) {
+// 	this.name = name;
+// 	this.age = age;
+// }
+
+// Person.prototype.greet = function(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+// }
+
+// function Employee(name, age, jobTitle) {
+// 	this.name = name;
+// 	this.age = age;
+// 	this.jobTitle = jobTitle;
+
+// 	__proto__=Person;
+// }
+
+// Employee.prototype.jobGreet = function(){
+//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// }
 
 // Do not change code below this line
 window.Person = Person;
