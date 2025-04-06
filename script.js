e07a40e2fb753f792650ca4de657fd61d6f0ccf2
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-07 01:31:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,52 +21,52 @@
 // 	}
 // }
 
-class Person {
-	constructor(name, age) {
-		this.name = name;
-		this.age = age;
-	}
+// class Person {
+// 	constructor(name, age) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
 
-	greet() {
-		const message = `Hello, my name is ${this.name}, I am ${this.age} years old.`;
-		console.log(message);
-		return message;  // ✅ Returning the message helps Cypress validate the output
-	}
+// 	greet() {
+// 		const message = `Hello, my name is ${this.name}, I am ${this.age} years old.`;
+// 		console.log(message);
+// 		return message;  // ✅ Returning the message helps Cypress validate the output
+// 	}
+// }
+
+// class Employee extends Person {
+// 	constructor(name, age, jobTitle) {
+// 		super(name, age);
+// 		this.jobTitle = jobTitle;
+// 	}
+
+// 	jobGreet() {
+// 		const message = `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
+// 		console.log(message);
+// 		return message;  // ✅ Helps Cypress verify the correct output
+// 	}
+// }
+
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
 }
 
-class Employee extends Person {
-	constructor(name, age, jobTitle) {
-		super(name, age);
-		this.jobTitle = jobTitle;
-	}
-
-	jobGreet() {
-		const message = `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`;
-		console.log(message);
-		return message;  // ✅ Helps Cypress verify the correct output
-	}
+Person.prototype.greet = function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 }
 
-// function Person(name, age) {
-// 	this.name = name;
-// 	this.age = age;
-// }
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
 
-// Person.prototype.greet = function(){
-// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-// }
+	__proto__=Person;
+}
 
-// function Employee(name, age, jobTitle) {
-// 	this.name = name;
-// 	this.age = age;
-// 	this.jobTitle = jobTitle;
-
-// 	__proto__=Person;
-// }
-
-// Employee.prototype.jobGreet = function(){
-//     console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-// }
+Employee.prototype.jobGreet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+}
 
 // Do not change code below this line
 window.Person = Person;
