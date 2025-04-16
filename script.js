c7401b6462225631ad5efdbd4140257da562172e
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-04-16 15:32:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -52,8 +52,8 @@ function Person(name, age) {
 	this.age = age;
 }
 
-Person.prototype.greet = function(){
-	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+Person.prototype.greet(){
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
 
 function Employee(name, age, jobTitle) {
